578e395c67f6b7e4c3ab597778d8132660123a7c
abhibhave <abhijit.bhave@gmail.com>
2020-02-22 14:47:18 -0800

    Assignment 5

--- src/main/java/database/DatabaseHelper.java
+++ src/main/java/database/DatabaseHelper.java
@@ -20,41 +20,53 @@ public class DatabaseHelper {
 
     //Creating a database schema post checking if one already exists.
     public void createWeatherDatabase() {
-        System.out.println("Checking if the database table already exists.");
-        if (!weatherTableExists()) {
-            System.out.println("The required tables don't exist.\nCreating new tables.");
-            //A simple create statement for the various schema items.
-            String sql = "create table WEATHER (USER_ID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1),"
-                + "userFirstName varchar(25), userLastName varchar(25), "
-                + "selectedWeatherConditions varchar(50), userContactId varchar(200), userContactPreference varchar(20), zipCode INT, isdeleted INT)";
+        System.out.println("Checking if the database table already exists.\n");
             //Leveraging try with functionality to connect to the database and create the schema.
             try (Connection databaseConnection = DriverManager.getConnection(dbUrl)) {
-                databaseConnection.createStatement().execute(sql);
-                System.out.println("Tables created");
+                //Checking if the User Details table exists. If it does not will create a new one.
+                if(!weatherTableExists( "userDetails"))
+                {
+                    databaseConnection.createStatement().execute("CREATE TABLE userDetails (USERID VARCHAR(255), userFirstName VARCHAR(25), "
+                        + "userLastName VARCHAR(25), userContactPreference VARCHAR(20), zipCode INT, isdeleted INT)");
+                    System.out.println("Created UserDetails table");
+                }
+                else{
+                    System.out.println("UserDetails Table exists, skipping creation");
+                }
+                //Checking if the User Contact Details table exists. If it does not will create a new one.
+                if(!weatherTableExists( "UserContactDetails")) {
+                    databaseConnection.createStatement().execute("CREATE TABLE UserContactDetails(USERID VARCHAR(255), selectedWeatherConditions VARCHAR(50), "
+                        + "userContactId VARCHAR(200))");
+                    System.out.println("Created UserContactDetails table");
+                }
+                else{
+                    System.out.println("UserContactDetails Table exists, skipping creation\n");
+                }
 
             } catch (SQLException e) {
                 e.printStackTrace();
             }
-        //If its detected that the schema already exists then skipping creation of the database schema.
-        } else {
-            System.out.println("Required tables exist. Skipping table creation.");
-        }
+
     }
 
     //A method to insert data into the database. Note that we can leverage frameworks such as hibernate here, however given the scope of this application is very small
     //I am using insert statements directly. However once the scope increases from just weather items, we should certainly look at frameworks.
+    //Note that I am performing two separate inserts at this point however if the tables numbers grow we need to consider frameworks like hibernate.
     public Boolean insertIntoDatabase(UserPreferences userPreferences) {
             try (Connection databaseConnection = DriverManager.getConnection(dbUrl)) {
                 StringBuilder sb = new StringBuilder();
+                Boolean successfulInsert = false;
                 //Using a simple Lambda to iterate over all weather conditions and put them into a String for insertion.
-                userPreferences.getSelectedWeatherConditions().forEach((weather) -> sb.append(weather));
-                String sql =
-                    "insert into WEATHER (userFirstName, userLastName, selectedWeatherConditions, userContactId, userContactPreference, zipCode, isDeleted) values  ('"
-                        + userPreferences.getUserFirstName() + "', '" + userPreferences.getUserLastName() + "', '" + sb.toString() + "', '"
-                        + userPreferences.getUserContactId() + "', '" + userPreferences.getUserContactPreference() + "', " + userPreferences.getZipCode() + ", 0)";
+                userPreferences.getSelectedWeatherConditions().forEach((weather) -> sb.append(weather + " "));
                 //  Returning a boolean of weather the operation completed successfully.
-                return (databaseConnection.createStatement().execute(sql));
-
+                successfulInsert = (databaseConnection.createStatement().execute("insert into UserDetails (userId, userFirstName, userLastName,  userContactPreference, zipCode, isDeleted) values  "
+                    + "('" + userPreferences.getUserId() + "', '" + userPreferences.getUserFirstName() + "', "
+                    + "'" + userPreferences.getUserLastName() + "', '" + userPreferences.getUserContactPreference() + "', "
+                    + "" + userPreferences.getZipCode() + ", 0)"));
+                successfulInsert = (databaseConnection.createStatement().execute("insert into UserContactDetails (userId,  selectedWeatherConditions, userContactId) values  "
+                    + "('" + userPreferences.getUserId() + "', '" + sb.toString() + "', '"
+                    + userPreferences.getUserContactId() + "')"));
+                return successfulInsert;
             } catch (SQLException e) {
                 e.printStackTrace();
             }
@@ -65,10 +77,14 @@ public class DatabaseHelper {
     // I am now leveraging ArrayLists of UserPreferences to be read and passed back to the calling function.
     public ArrayList<UserPreferences> readDataFromDatabase() {
         try (Connection databaseConnection = DriverManager.getConnection(dbUrl)) {
-            //The select statement to bring in all the rows where isDeleted is not true.
-            String sql = "Select * from weather where isdeleted = 0 order by User_ID";
+            //The select statement to bring in all the rows where isDeleted is not true. Note that the select statement contains a join and is reading data
+            //from both the tables.
             //Reading the results into resultSet.
-            ResultSet resultSet = databaseConnection.createStatement().executeQuery(sql);
+            ResultSet resultSet = databaseConnection.createStatement().executeQuery("SELECT ud.USERID, ud.UserFirstName, ud.UserLastName, ucd.selectedWeatherConditions, ucd.userContactId, "
+                + "ud.userContactPreference, ud.zipCode "
+                + "FROM UserDetails ud JOIN UserContactDetails ucd ON ud.UserId = ucd.UserID "
+                + "WHERE ud.isDeleted = 0 "
+                + "ORDER BY ud.UserLastName");
             ArrayList<UserPreferences> userPreferencesList = new ArrayList<>();
            //Enriching the Arraylist of userPreferences.
             while (resultSet.next()) {
@@ -95,14 +111,14 @@ public class DatabaseHelper {
     }
 
     //Method to check if the schema exists.
-    public boolean weatherTableExists() {
+    public boolean weatherTableExists(String tableName) {
         boolean isTableCreated = false;
-        //leveraging the try with strucutre to connect to the database.
-        try (Connection databaseConnection = DriverManager.getConnection(dbUrl)) {
+        //leveraging the try with structure to connect to the database.
+        try (Connection databaseConnection = DriverManager.getConnection(dbUrl)){
             DatabaseMetaData dbMetaData = databaseConnection.getMetaData();
             ResultSet metaDataResultSet = dbMetaData.getTables(null, null, null, new String[] {"TABLE"});
             while (metaDataResultSet.next()) {
-                if (metaDataResultSet.getString(3).toUpperCase().matches("WEATHER")) {
+                if (metaDataResultSet.getString(3).toUpperCase().matches(tableName.toUpperCase())) {
                     isTableCreated = true;
                 }
             }
@@ -119,9 +135,9 @@ public class DatabaseHelper {
     //Method to delete the data from the database. Note that this method only soft deletes the data in the database.
     //A hard delete would need to be performed by actually purging all the rows that are marked with is_delete = 1; This will be done manually.
     //Leveraging try with structure for connection to the database.
-    public Boolean deleteDataFromDatabase(Integer userID) {
+    public Boolean deleteDataFromDatabase(String userID) {
         try (Connection databaseConnection = DriverManager.getConnection(dbUrl)) {
-            String sql = "Update weather set isdeleted = 1 where user_Id = " + userID;
+            String sql = "Update userDetails set isdeleted = 1 where userId = '" + userID+ "'";
             return (databaseConnection.createStatement().execute(sql));
         } catch (SQLException e) {
             e.printStackTrace();
